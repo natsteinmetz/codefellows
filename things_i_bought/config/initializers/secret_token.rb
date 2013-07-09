407b861b17db4762424ccd35aa714121f27b4e31
Nathalie Steinmetz <nathalie.steinmetz@gmail.com>
2013-07-09 15:36:08 -0700

    Homework 1 & 2 from 'Rails 4 in Action'
    Homework 1 - 'things_i_bought'
    Homework 2 - 'bacon' (yay, saved the bacon!)

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = 'b1f0944a869505d0e5845c0b98eec845d7c39f057f62fe142ab7bd4cea3bb34397e0264d7091447043a7e422e340fdac87716b8859f1e98cc4d01d419dd2e1b1'
